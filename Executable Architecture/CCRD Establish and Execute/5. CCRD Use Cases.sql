6cdbe1287582a91e7ef23d23b16fdb2937d9bab5
Kris Simpson <krissimpson@Kriss-MacBook-Pro.local>
2022-06-01 21:35:51 +1000

    Updated CCRD case for LCAM

--- Executable Architecture/CCRD Establish and Execute/5. CCRD Use Cases.sql
+++ Executable Architecture/CCRD Establish and Execute/5. CCRD Use Cases.sql
@@ -12,5 +12,4 @@ Union All
 select converter.ccrd(15,'¬∞C','Soil Temperature',0);
 
 
-select *,converter.ccrd_mod(1, ptd.uom, ptd.metric,ptd.value) as user_id_1,converter.ccrd_mod(9, ptd.uom, ptd.metric,ptd.value) as user_id_9,converter.ccrd_mod(15, ptd.uom, ptd.metric,ptd.value) as user_id_15 from pairtreetestdata ptd limit 100;
-select *,converter.ccrd(1, ptd.uom, ptd.metric,ptd.value) as user_id_1,converter.ccrd(9, ptd.uom, ptd.metric,ptd.value) as user_id_9,converter.ccrd(15, ptd.uom, ptd.metric,ptd.value) as user_id_15 from pairtreetestdata ptd limit 100;
+select *,converter.ccrd_final(1, ptd.uom, ptd.metric,ptd.value) as user_id_1,converter.ccrd_mod(9, ptd.uom, ptd.metric,ptd.value) as user_id_9,converter.ccrd_mod(15, ptd.uom, ptd.metric,ptd.value) as user_id_15 from pairtreetestdata ptd limit 100;
